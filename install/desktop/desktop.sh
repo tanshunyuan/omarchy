633381c09dbe318a31e814b83395ebae731efe31
Shun Yuan <tanshunyuan99@gmail.com>
2025-08-15 15:37:03 +0800

    chore: update

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-yay -S --noconfirm --needed \
-  brightnessctl playerctl pamixer wiremix wireplumber \
-  fcitx5 fcitx5-gtk fcitx5-qt wl-clip-persist \
-  nautilus sushi ffmpegthumbnailer gvfs-mtp \
-  slurp satty \
-  mpv evince imv \
-  chromium
+pkgs=(
+  brightnessctl         # brightness control
+  playerctl            # media player control
+  pamixer              # pulseaudio mixer
+  pavucontrol          # pulseaudio volume control
+  wireplumber          # pipewire session manager
+  fcitx5               # input method framework
+  fcitx5-gtk           # fcitx5 GTK integration
+  fcitx5-qt            # fcitx5 Qt integration
+  fcitx5-configtool    # fcitx5 configuration tool
+  wl-clip-persist      # wayland clipboard persistence
+  # nautilus             # file manager
+  # sushi                # file previewer
+  # ffmpegthumbnailer    # video thumbnails
+  mpv                  # video player
+  evince               # document viewer
+  imv                  # image viewer
+  firefox-developer-edition             # web browser
+  zen-browser-bin
+  slack-desktop
+  telegram-desktop
+)
+yay -S --noconfirm --needed "${pkgs[@]}"
 
 # Add screen recorder based on GPU
 if lspci | grep -qi 'nvidia'; then
